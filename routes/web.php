9a7a8f75ea47a6f92ef17d9531abc810dd5c8057
Jorge Martinez <a18311030@uthermosillo.edu.mx>
2026-03-12 22:33:42 -0700

    fix: resolver conflicto de merge en routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FavoritoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/favoritos', [FavoritoController::class, 'index'])->name('favoritos');
+    Route::post('/favoritos/{libro}/toggle', [FavoritoController::class, 'toggle'])->name('favoritos.toggle');
 });
 
 Route::get('/login', function () {
